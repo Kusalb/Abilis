3beb763bb512722ff2db2aaf2c526fa228993e2f
Kusal Bista <grand.kusal.kb@gmail.com>
2020-07-19 21:17:32 +0545

    Update settings.py

--- Abilis/settings.py
+++ Abilis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$p*_s5hh4uz981ta18(n-^wls280jydqlz5tyww6txsr208!f1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -145,8 +145,8 @@ FCM_DJANGO_SETTINGS = {
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'grand.kusal.kb@gmail.com'
-# EMAIL_HOST_PASSWORD = 'grandprime'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
 # EMAIL_USE_TLS = True
 # # EMAIL_USE_SSL = False
 # ACCOUNT_EMAIL_VERIFICATION     = 'none'
